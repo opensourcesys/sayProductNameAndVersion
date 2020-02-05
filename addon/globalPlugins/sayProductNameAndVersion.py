15697e317a506b6ac4917149664aeda0ae331677
Patrick ZAJDA <patrick@zajda.fr>
2020-02-05 12:11:14 +0100

    Make the plugin compatible with NVDA 2019.3 and above. Also, get version information from NVDA itself instead of using win32Process.

---
+++ addon/globalPlugins/sayProductNameAndVersion.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# Version Info Of Current program announcement Global Plugin for NVDA
+# Authors: Patrick ZAJDA
+# Shortcut: NVDA+Shift+V
+# Press it twice to copy version informations to the clipboard
+
+import addonHandler
+import scriptHandler
+import globalPluginHandler
+from ui import message
+import api
+from globalCommands import SCRCAT_SPEECH
+
+# initialize translations
+addonHandler.initTranslation()
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+	# We initialize the scripts category shown on input gestures dialog
+	scriptCategory = SCRCAT_SPEECH
+
+	def script_sayProductNameAndVersion(self, gesture):
+		focus=api.getFocusObject()
+		try:
+			productName = focus.appModule.productName
+		except Exception:
+			productName = None
+		try:
+			productVersion = focus.appModule.productVersion
+		except Exception:
+			productVersion = None
+
+		if productName is None or productName=="":
+			# Translators: This is used when the name of the focused application cannot be found.
+			productName = _("Application")
+		if productName is not None and productVersion is not None and productName != "" and productVersion != "":
+			isSameScript =scriptHandler.getLastScriptRepeatCount()
+			if isSameScript==0:
+				# Translators: This is the message which will be spoken or copied to the clipboard. {name} is the app name, {version} its the version.
+				message(_("{name} version {version}").format(name=productName,version=productVersion))
+			else:
+				if api.copyToClip(_("{name} version {version}").format(name=productName,version=productVersion)):
+					# Translators: This is the message announced when all informations has been copied.
+					message(_(u"{name} version {version} copied to the clipboard").format(name=productName,version=productVersion))
+				else:
+					# Translators: This is the message announced when all informations hasn't been copied.
+					message(_("Cannot copy version informations to the clipboard."))
+		else:
+			# Translators: this will be spoken if version informations were not available.
+			message(_("Unable to get version info"))
+
+	# Documentation
+	# Translators: Input help mode message for say product name and version command.
+	script_sayProductNameAndVersion.__doc__ = _("Announce the version of the executable of the focused window or copy it to the clipboard if pressed twice")
+
+	__gestures={
+		"kb:NVDA+Shift+V": "sayProductNameAndVersion",
+	}
